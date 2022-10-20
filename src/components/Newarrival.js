3a515e251b1aec5148c00d26bd99694e2d695bae
Yeto2 <bota.tll2004@gmail.com>
2022-10-20 23:48:34 +0100

    adding New-arrival section and data file

---
+++ src/components/Newarrival.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Newarrival() {
+    return (
+    <div className='card'>
+        <div className='arrival-sec'>
+            <img src='' />
+            <p>Add To Card</p>
+            <div className=''></div>
+        </div>
+        <div className='arrival-text'>
+            <p></p>
+            <span></span>
+        </div>
+    </div>
+    )
+}
+
+export default Newarrival
