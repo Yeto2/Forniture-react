b3f882fd33626052272728307e21ee6e19e252c8
Yeto2 <bota.tll2004@gmail.com>
2022-10-24 15:13:13 +0100

    finishing collection section

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -7,24 +7,31 @@ function Collection() {
     return <div className="owl-carousel">
         <div className="col-element1">
             {/* <img src={sofa} /> */}
+            <span className="rect"></span>
             <div className="collection-txt container">
-                <h2>unlimited sofa collection</h2>
+                <h3>unlimited sofa collection</h3>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit,
                     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                 </p>
+                <b>Starting from <span>$ 199</span></b>
+                <button className="one">View More</button>
+
             </div>
         </div>
         <div className="col-element2">
             {/* <img src={table} /> */}
+            <span className="rect"></span>
             <div className="collection-txt container">
-                <h2>unlimited dainning table collection</h2>
+                <h3>unlimited dainning table collection</h3>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit,
                     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                 </p>
+                <b>Starting from <span>$ 299</span></b>
+                <button className="one">View More</button>
             </div>
         </div>
 
